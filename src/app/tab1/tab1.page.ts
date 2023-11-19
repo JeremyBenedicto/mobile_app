fb606356b64dbcafff85ffef3a206540dcc94a88
Jeremy Benedicto <jeremybenedicto2020@gmail.com>
2023-11-19 17:20:12 +0800

    d

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+
+  constructor() {
+
+}
+
+
+  swiperSlideChanged (e: any) {
+    console.log ('changed: ', e);
+  }
 
 }
